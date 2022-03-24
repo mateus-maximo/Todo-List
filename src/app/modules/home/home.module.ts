d2d6c3944e90e38d92810b8f10539d732a2dafbb
Mateus <mateushen19@outlook.com>
2022-03-24 14:26:39 -0300

    Criando componente header

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
