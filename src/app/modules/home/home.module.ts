0b8217f5ee4b8769c4ddc9a317ed9fa7ee31725e
Mateus <mateushen19@outlook.com>
2022-03-24 08:32:33 -0300

    Criando page home

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule
   ]
