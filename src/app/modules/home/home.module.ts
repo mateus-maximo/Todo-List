c1a0375b6d280dfcbc425a675ec796a536c726a7
Mateus <mateushen19@outlook.com>
2022-03-24 14:44:50 -0300

    Adicionando componente todo-list

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    TodoListComponent
   ],
   imports: [
     CommonModule
